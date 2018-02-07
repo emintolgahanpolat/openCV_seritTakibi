eb2f0a428c64f8a79c17bb914ea02b56019e7dd9
Emin Tolgahan POLAT <emintolgahanpolat@gmail.com>
2018-02-07 11:34:49 +0300

    center edit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import track
 import detect
 
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 def main(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -36,29 +37,51 @@ def main(video_path):
         lanes = ld.detect(frame)
 
             #goruntu ortasi
-        cv2.line(frame, (int(width/2),height), (int(width/2),0 ), (255, 255, 255), 2)
+        cv2.line(frame, (int(width/2),0), (int(width/2),height ), (255, 255, 255), 2)
 
 
         if predicted is not None:
+            Ax=predicted[0][2]
+            Ay=predicted[0][3]
+
+            Bx=predicted[0][0]
+            By=predicted[0][1]
+
+            Cx=predicted[1][2]
+            Cy=predicted[1][3]
+
+            Dx=predicted[1][0]
+            Dy=predicted[1][1]
+
             #koseler
-            cv2.line(frame, (predicted[0][0], predicted[0][1]), (predicted[0][2], predicted[0][3]), (0, 255, 255), 5)
-            cv2.line(frame, (predicted[1][0], predicted[1][1]), (predicted[1][2], predicted[1][3]), (0, 255, 255), 5)
+            cv2.line(frame, (Ax, Ay), (Bx,By), (0, 255, 255), 5)
+            cv2.line(frame, (Cx, Cy), (Dx,Dy), (0, 255, 255), 5)
 
             #alan
             ic = np.array(
                 [
-                [predicted[0][0],predicted[0][1]],
-                [predicted[0][2],predicted[0][3]],
-                [predicted[1][0],predicted[1][1]],
-                [predicted[1][2],predicted[1][3]]
+                [Ax,Ay],
+                [Bx,By],
+                [Cx,Cy],
+                [Dx,Dy]
                 ],
                 np.int32)
 
             #alanin ortasi
-            cv2.line(frame, 
-            ((predicted[1][0]+predicted[0][2])/2,(predicted[1][1]+predicted[0][3])/2),
-             ((predicted[0][0]+predicted[1][2])/2, (predicted[0][1]+predicted[1][3])/2), (0, 0, 255), 5)
-           
+            cv2.line(frame,((Ax+Dx)/2,(Ay+Dy)/2),((Bx+Cx)/2, (By+Cy)/2),(0, 0, 255), 5)
+
+            cv2.circle (frame,((Ax+Dx)/2,(Ay+Dy)/2),5,(255,0,0),-1)
+
+            cv2.line(frame,((Ax+Dx)/2,(Ay+Dy)/2),(int(width/2), (Ay+Dy)/2),(0, 255, 0), 3)
+       
+            veri="s"+str(((Ax+Dx)/2)-(width/2))
+            cv2.putText(frame,veri, ((Ax+Dx)/2,(Ay+Dy)/2), font, 0.5, (255,255,255),1,cv2.LINE_AA)
+
+            #kose isimleri
+            cv2.putText(frame,'A', (Ax,Ay), font, 0.5, (255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'B', (Bx,By), font, 0.5, (255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'C', (Cx,Cy), font, 0.5, (255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'D', (Dx,Dy), font, 0.5, (255,255,255),1,cv2.LINE_AA)
 
 
 
